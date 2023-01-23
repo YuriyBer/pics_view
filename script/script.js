6bfc69be9a8a7a4c19c603ac1db9a77d76dfb834
YuriyBer <hospitalier82@mail.ru>
2023-01-23 21:29:07 +0300

    ok

---
+++ script/script.js
@@ -0,0 +1,48 @@
+var prev = document.querySelector(".prev");
+prev.addEventListener("click", function () {
+    plusSlides(-1);
+});
+var next = document.querySelector(".next");
+next.addEventListener("click", function (e) {
+    plusSlides(1);
+});
+var dotsField = document.querySelector(".dots");
+dotsField.addEventListener("click", function (e) {
+    if (e.target instanceof HTMLElement) {
+        if (e.target.classList.contains("dot")) {
+            currentSlide(Number(e.target.dataset.num));
+        }
+    }
+});
+var slideIndex = 0;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+    slideIndex = n;
+    showSlides(slideIndex);
+}
+var interval = 3;
+var slideShow = setInterval(function (interval) {
+    plusSlides(1);
+}, interval * 1000);
+document.addEventListener("click", function () {
+    clearInterval(slideShow);
+});
+function showSlides(n) {
+    var slides = document.querySelectorAll(".mySlides");
+    var dots = document.querySelectorAll(".dot");
+    if (n > slides.length - 1) {
+        slideIndex = 0;
+    }
+    else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
---
+++ script/script.ts
@@ -0,0 +1,58 @@
+const prev: HTMLElement = document.querySelector(".prev");
+prev.addEventListener("click", () => {
+  plusSlides(-1);
+});
+const next: HTMLElement = document.querySelector(".next");
+next.addEventListener("click", function (e) {
+  plusSlides(1);
+});
+
+const dotsField: HTMLElement = document.querySelector(".dots");
+
+dotsField.addEventListener("click", (e) => {
+  if (e.target instanceof HTMLElement) {
+    if (e.target.classList.contains("dot")) {
+      currentSlide(Number(e.target.dataset.num));
+    }
+  }
+});
+
+let slideIndex: number = 0;
+showSlides(slideIndex);
+
+function plusSlides(n: number) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n: number) {
+  slideIndex = n;
+  showSlides(slideIndex);
+}
+
+let interval: number = 3;
+const slideShow = setInterval((interval: number) => {
+  plusSlides(1);
+}, interval * 1000);
+
+document.addEventListener("click", () => {
+  clearInterval(slideShow);
+});
+
+function showSlides(n: number) {
+  const slides: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".mySlides");
+  const dots: NodeListOf<HTMLElement> = document.querySelectorAll(".dot");
+
+  if (n > slides.length - 1) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i: number = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
